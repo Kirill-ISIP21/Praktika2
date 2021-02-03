d70848a20ba13eaf6d50179f1a08020e179f8a46
k.chukharev@mail.ru <k.chukharev@mail.ru>
2021-02-03 20:24:06 +0300

    Добавьте файлы проекта.

---
+++ Praktika2/Classes/Calculate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika2
+{
+    class Calculate
+    {
+        public Calculate(int rozes, int tulpanes)
+        {
+        this.rozes = rozes;
+        this.tulpanes = tulpanes;
+        }
+
+       public int rozes { get; set; }
+       public int tulpanes { get; set; }
+       public string allPrice()
+       {
+          int priceRoz = rozes * 50;
+          int priceTulp = tulpanes * 30;
+            if (priceRoz > priceTulp)
+                {
+                    return $"У цветочницы с розами выручка больше: {priceRoz}, а с тюльпанами: {priceTulp}";
+                }
+                else if (priceRoz < priceTulp)
+                {
+                    return $"У цветочницы с тюльпанами  выручка больше: {priceTulp}, а с розами: {priceRoz}";
+                }
+            else
+            {
+                return $"Выручка у обеих цветочниц одинаковая {priceRoz}";
+            }
+       }
+    }
+}
---
+++ Praktika2/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Praktika2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            int rozes, tulpanes;
+            int.TryParse(tBoxRoz.Text, out rozes);
+            int.TryParse(tBoxTulp.Text, out tulpanes);
+            Calculate answer = new Calculate(rozes, tulpanes);
+            lblAnswer.Content = answer.allPrice().ToString();
+            if (tBoxRoz.Text=="" && tBoxTulp.Text == "")
+            {
+                lblAnswer.Content = "Введите кол-во цветов";
+            }
+
+        }
+        private void tBoxRoz_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            //отключает возможность писать в TextBox буквы и символы
+            if (!Char.IsDigit(e.Text, 0))
+            {
+                e.Handled = true;
+            }
+        }
+        private void tBoxTulp_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
